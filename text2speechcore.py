198c9a97d3e26b8d86b21906a3a4dc9d6f8f6d28
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-07-29 16:30:16 -0400

    Update text2speechcore.py

--- text2speechcore.py
+++ text2speechcore.py
@@ -1,10 +1,12 @@
 import os
 
 def start():
-    print("Text2Speech - Make sure you use quotations!")
+    print("Text2Speech - Make sure you use quotations! Type :end to end!")
     print
     while True:
         a = str(input(">"))
+        if (a == ":end"):
+            break
         a = "say " + a
         os.system(a)
 
